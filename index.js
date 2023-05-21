c28c8651306495e6595ac3d2a0e6a63c1541d362
Mariano <lmarianoescobar11@gmail.com>
2023-05-21 14:58:57 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const titulo= document.getElementById("titulo");
-const foto = document.getElementById("foto");
-const perrafo= document.getElementById("parrafo");
+
+
+const contenedor =document.getElementById("container")
+
+const titulo = document.getElementById("titulo").value;
+const imagen= document.getElementById("foto").value;
+const descripcion =document.getElementById("parrafo").value;
+
+const  boton= document.getElementById("btn");
+
+boton.addEventListener("click",handleClick);
+function handleClick(){
+
+const card = document.createElement("div");
+card= innerHTML= `
+<h2>${titulo }</h2>
+<img src=${imagen } alt=${titulo}>
+`
+contenedor.appendChild(card)};
