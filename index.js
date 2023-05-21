c0ca5ac5cb91f105aaac458c6383417a7574d623
Mariano <lmarianoescobar11@gmail.com>
2023-05-21 16:52:28 -0300

    tercer commit

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const titulo = document.getElementById("titulo").value;
 const imagen= document.getElementById("foto").value;
 const descripcion =document.getElementById("parrafo").value;
 
-const  boton= document.getElementById("btn");
+const  btn= document.querySelector("button");
 
-boton.addEventListener("click",handleClick);
-function handleClick(){
-
-const card = document.createElement("div");
-card= innerHTML= `
-<h2>${titulo }</h2>
-<img src=${imagen } alt=${titulo}>
-`
-contenedor.appendChild(card)};
+btn.addEventListener("click",handleClick);
+function handleClick() { 
+    
+    let card = document.createElement("div");
+    card= innerHTML=`
+    <h2>${titulo } </h2>
+    <img src=${imagen } alt=${titulo}>
+    <p> ${descripcion}</p>`}
+contenedor.appendChild(card);
+console.log(card);
